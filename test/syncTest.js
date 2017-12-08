ffc39de33d4195c3591d2261a54950bf03d46bb7
AAMLLe <myanh@moneylover.me>
2017-12-08 17:47:00 +0700

    add

--- test/syncTest.js
+++ test/syncTest.js
@@ -3,3 +3,5 @@ exports['test that logs all failures'] = function(assert) {
   assert.equal(2 + 2, 5, 'assert failure is logged')
   assert.equal(3 + 2, 5, 'assert pass is logged')
 }
+ 
+if (module == require.main) require('test').run(exports)
