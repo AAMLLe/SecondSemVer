7b5aeaeb6567301b2b36ee322411f808942c2108
AAMLLe <myanh@moneylover.me>
2017-12-12 14:03:23 +0700

    Breaking: need 2 params for fun anotherMsg

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.printMsg = function() {
  console.log("Here I am!".green);
 }
 
-exports.anotherMsg = function(msg) {
- console.log(msg.cyan);
+exports.anotherMsg = function(msg, colorString) {
+ console.log(msg.colorString);
 }
