8dfe9a566b046a5174104f61532b7b622da03467
AAMLLe <myanh@moneylover.me>
2017-12-13 18:18:15 +0700

    update index code  2

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exports.anotherMsg = function(msg, color, repeatTimes) {
 }
 
 exports.welcomeMsg = function() {
-console.log("ふぁんど".green);
+console.log("ふぁんど".darkGreen);
 }
 
 exports.welcomeMsg = function(color) {
