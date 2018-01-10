1eef69146add7d1616277752ad6300a5cd589e61
Mrini <myanh@moneylover.me>
2018-01-10 21:13:04 +0700

    fix travis file 6

--- 3excB.sh
+++ 3excB.sh
@@ -8,7 +8,8 @@ commit_files() {
   git add . *.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+# git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+commit_files
 git push -fq origin master > /dev/null
 git push --follow-tags origin master
 echo `chandler push`
