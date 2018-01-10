4a4a97c2825c825535eb0f4ecb156031c212dc94
Mrini <myanh@moneylover.me>
2018-01-10 21:22:34 +0700

    fix travis file 7

--- 3excB.sh
+++ 3excB.sh
@@ -2,12 +2,13 @@
 # execute using backticks
 git checkout -b master
 echo `npm run releaseTh`
-git rm -r --cached node_modules
+
 commit_files() {
   # git checkout -b master
-  git add . *.md
+  git add CHANGELOG.md
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
+git rm -r --cached node_modules/
 # git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
 commit_files
 git push -fq origin master > /dev/null
