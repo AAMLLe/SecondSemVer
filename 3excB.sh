e94a9ba32befe13834685c5976a30aff342cbd46
Mrini <myanh@moneylover.me>
2018-01-10 18:27:30 +0700

    fix script file

--- 3excB.sh
+++ 3excB.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # execute using backticks
-echo `npm run releaseTh; git push --follow-tags origin master; chandler push`
+echo `npm run releaseTh`
+git add -f .
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci] "
+git push -fq origin master > /dev/null
+git push --follow-tags origin master
+echo `chandler push`
